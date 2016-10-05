a2bd7340cf64f240add0fedaebf1ff3c2c130937
orz025846 <ec_mail02@yahoo.co.jp>
2016-10-05 22:11:46 +0900

    2Dゲームアプリを作ろう／ ゲームを再生してみよう／ キューブの接触時に効果音をつけましょう Lesson7の課題提出です。課題作業用追加＞CubeController.cs（追記）

---
+++ Assets/CubeController.cs
@@ -0,0 +1,43 @@
+﻿// 課題---★
+using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+       // 消滅位置
+    private float deadLine = -10;
+    
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+        //Debug.Log(this.transform.position);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    // ★キューブが何かに接触したときに呼ばれる関数
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        GetComponent<AudioSource>().Play();
+
+        if (other.gameObject.tag == "Unitychan")
+        {
+            GetComponent<AudioSource>().volume = 0;
+        }
+    }
+}
